f2cef91c7f74986dde7a76478b886493a08f9931
Oxana <oxkurilenko@yandex.ru>
2024-08-12 10:28:36 +0300

    правки 1

--- task3.py
+++ task3.py
@@ -2,25 +2,25 @@ date = int(input('Введите день рождения:'))
 month =(input('Введите месяц рождения:'))
 if ((month) == ('март')and (date)>=21) or ((month) == ('апрель')and (date)<=20):
     print ('Ваш знак зодиака: Овен')
-if ((month) == ('апрель') and (date) >= 21) or ((month) == ('май') and (date) <= 20):
+elif ((month) == ('апрель') and (date) >= 21) or ((month) == ('май') and (date) <= 20):
     print('Ваш знак зодиака: Телец')
-if ((month) == ('май') and (date) >= 22) or ((month) == ('июнь') and (date) <= 21):
+elif ((month) == ('май') and (date) >= 22) or ((month) == ('июнь') and (date) <= 21):
     print('Ваш знак зодиака: Близнецы')
-if ((month) == ('июнь') and (date) >= 22) or ((month) == ('июль') and (date) <= 22):
+elif ((month) == ('июнь') and (date) >= 22) or ((month) == ('июль') and (date) <= 22):
     print('Ваш знак зодиака: Рак')
-if ((month) == ('июль') and (date) >= 23) or ((month) == ('август') and (date) <= 23):
+elif ((month) == ('июль') and (date) >= 23) or ((month) == ('август') and (date) <= 23):
     print('Ваш знак зодиака: Лев')
-if ((month) == ('август') and (date) >= 22) or ((month) == ('сентябрь') and (date) <= 23):
+elif ((month) == ('август') and (date) >= 22) or ((month) == ('сентябрь') and (date) <= 23):
     print('Ваш знак зодиака: Дева')
-if ((month) == ('сентябрь') and (date) >= 24) or ((month) == ('октябрь') and (date) <= 23):
+elif ((month) == ('сентябрь') and (date) >= 24) or ((month) == ('октябрь') and (date) <= 23):
     print('Ваш знак зодиака: Весы')
-if ((month) == ('октябрь') and (date) >= 24) or ((month) == ('ноябрь') and (date) <= 22):
+elif ((month) == ('октябрь') and (date) >= 24) or ((month) == ('ноябрь') and (date) <= 22):
     print('Ваш знак зодиака: Скорпион')
-if ((month) == ('ноябрь') and (date) >= 23) or ((month) == ('декабрь') and (date) <= 22):
+elif ((month) == ('ноябрь') and (date) >= 23) or ((month) == ('декабрь') and (date) <= 22):
     print('Ваш знак зодиака: Стрелец')
-if ((month) == ('декабрь') and (date) >= 23) or ((month) == ('январь') and (date) <= 20):
+elif ((month) == ('декабрь') and (date) >= 23) or ((month) == ('январь') and (date) <= 20):
     print('Ваш знак зодиака: Козерог')
-if ((month) == ('январь') and (date) >= 21) or ((month) == ('февраль') and (date) <= 19):
+elif ((month) == ('январь') and (date) >= 21) or ((month) == ('февраль') and (date) <= 19):
     print('Ваш знак зодиака: Водолей')
-if ((month) == ('февраль') and (date) >= 20) or ((month) == ('март') and (date) <= 20):
+elif ((month) == ('февраль') and (date) >= 20) or ((month) == ('март') and (date) <= 20):
     print('Ваш знак зодиака: Рыбы')
