9f1f80a8b76934e5a13b5b6456605011b96f5f9d
Oxana <oxkurilenko@yandex.ru>
2024-08-12 10:26:15 +0300

    правки

--- task3.py
+++ task3.py
@@ -1,28 +1,26 @@
 date = int(input('Введите день рождения:'))
 month =(input('Введите месяц рождения:'))
-if (month) == ('декабрь')and (date)<=22 :
-     print ('Ваш знак зодиака: Стрелец')
-if (month) == ('январь') and (date)<=20 :
-     print ('Ваш знак зодиака: Козерог')
-if (month) == ('февраль') and (date)<=18 :
-     print ('Ваш знак зодиака: Водолей')
-if (month) == ('март') and (date)<=20 :
-     print ('Ваш знак зодиака: Рыба')
-if (month) == ('апрель') and (date)<=19 :
-     print ('Ваш знак зодиака: Овен')
-if (month) == ('май') and (date)<=20 :
-     print ('Ваш знак зодиака: Телец')
-if (month) == ('июнь') and (date)<=20 :
-     print ('Ваш знак зодиака: Близнецы')
-if (month) == ('июлю') and (date)<=22 :
-     print ('Ваш знак зодиака: Рак')
-if (month) == ('август') and (date)<=22 :
-     print ('Ваш знак зодиака: Лев')
-if (month) == ('сентябрь') and (date)<=22 :
-     print ('Ваш знак зодиака: Дева')
-if (month) == ('октябрь') and (date)<=22 :
-     print ('Ваш знак зодиака: Весы')
-if (month) == ('ноябрь') and (date)<=21 :
-     print ('Ваш знак зодиака: Скорпион')
-if (month) == ('ноябрь') and (date)>=22 :
-     print ('Ваш знак зодиака: Стрелец')
+if ((month) == ('март')and (date)>=21) or ((month) == ('апрель')and (date)<=20):
+    print ('Ваш знак зодиака: Овен')
+if ((month) == ('апрель') and (date) >= 21) or ((month) == ('май') and (date) <= 20):
+    print('Ваш знак зодиака: Телец')
+if ((month) == ('май') and (date) >= 22) or ((month) == ('июнь') and (date) <= 21):
+    print('Ваш знак зодиака: Близнецы')
+if ((month) == ('июнь') and (date) >= 22) or ((month) == ('июль') and (date) <= 22):
+    print('Ваш знак зодиака: Рак')
+if ((month) == ('июль') and (date) >= 23) or ((month) == ('август') and (date) <= 23):
+    print('Ваш знак зодиака: Лев')
+if ((month) == ('август') and (date) >= 22) or ((month) == ('сентябрь') and (date) <= 23):
+    print('Ваш знак зодиака: Дева')
+if ((month) == ('сентябрь') and (date) >= 24) or ((month) == ('октябрь') and (date) <= 23):
+    print('Ваш знак зодиака: Весы')
+if ((month) == ('октябрь') and (date) >= 24) or ((month) == ('ноябрь') and (date) <= 22):
+    print('Ваш знак зодиака: Скорпион')
+if ((month) == ('ноябрь') and (date) >= 23) or ((month) == ('декабрь') and (date) <= 22):
+    print('Ваш знак зодиака: Стрелец')
+if ((month) == ('декабрь') and (date) >= 23) or ((month) == ('январь') and (date) <= 20):
+    print('Ваш знак зодиака: Козерог')
+if ((month) == ('январь') and (date) >= 21) or ((month) == ('февраль') and (date) <= 19):
+    print('Ваш знак зодиака: Водолей')
+if ((month) == ('февраль') and (date) >= 20) or ((month) == ('март') and (date) <= 20):
+    print('Ваш знак зодиака: Рыбы')
